d700170f22efc13d13c177ef675426185bd7291f
Edward Lee <edwardlee0323@gmail.com>
2016-05-02 09:14:06 +0000

    Added Extra Challenge

--- snippets.py
+++ snippets.py
@@ -1,6 +1,7 @@
 import logging
 import argparse
 import psycopg2
+import psycopg2.extras
 
 logging.debug("Connecting to PostgreSQL")
 connection = psycopg2.connect(database="snippets")
@@ -9,20 +10,21 @@ logging.debug("Database connection established.")
 # Set the log output file, and the log level
 logging.basicConfig(filename="snippets.log", level = logging.DEBUG)
 
-def put(name, snippet):
+def put(name, snippet, hide=False):
     """
     Store a snippet with an associated name.
     """
     logging.info("Storing snippet {!r}: {!r}".format(name, snippet))
     with connection, connection.cursor() as cursor:
         try:
-            cursor.execute("insert into snippets values (%s, %s)", (name, snippet))
+            cursor.execute("INSERT INTO snippets values (%s, %s, %s)", (name, snippet, hide))
         except psycopg2.IntegrityError as e:
             connection.rollback()
-            cursor.execute("update snippets set message=%s where keyword=%s", (snippet, name))
+            cursor.execute("UPDATE snippets SET message=%s WHERE keyword=%s", (snippet, name))
     connection.commit()
     logging.debug("Snippet stored successfully.")
     return name, snippet
+
 
 def get(name):
     """
@@ -30,14 +32,39 @@ def get(name):
     """
     logging.info("Fetching snippet {!r}".format(name))
     with connection, connection.cursor() as cursor:
-        cursor.execute("select message from snippets where keyword=%s", (name, ))
+        cursor.execute("SELECT message FROM snippets WHERE \hidden=False AND keyword=%s", (name, ))
         row = cursor.fetchone()
     logging.debug("Snippet successfully fetched.")
     if not row:
         #no snippet was found with that name
         return "404: Snippet Not Found"
     return row[0]
-    
+
+
+def catalog():
+    """
+    Catalog to look up keywords
+    """
+    logging.info("Query Snippet")
+    with connection, connection.cursor(cursor_factory=psycopg2.extras.DictCursor) as cursor:
+        cursor.execute("SELECT keyword FROM snippets")
+        rows = cursor.fetchall()
+        for row in rows:
+            print(row["keyword"])
+
+
+
+def search(word):
+    """
+    Search for a string within snippet messages
+    """
+    logging.info("Search Function")
+    with connection, connection.cursor(cursor_factory=psycopg2.extras.DictCursor) as cursor:
+        cursor.execute("SELECT * FROM snippets WHERE message like '%%'||%s||'%%'", (word, ))
+        rows = cursor.fetchall()
+        for row in rows:
+            print(row['message'])
+
 
 def main():
     """Main function"""
@@ -51,11 +78,21 @@ def main():
     put_parser = subparsers.add_parser("put", help="Store a snippet")
     put_parser.add_argument("name", help="Name of the snippet")
     put_parser.add_argument("snippet", help="Snippet text")
+    put_parser.add_argument("--hide", help="Sets the column to hidden, not searchable with Search or Catalog commands")
 
     # Subparser for the get command
     logging.debug("Constructing get subparser")
     get_parser = subparsers.add_parser("get", help="Retrieve a snippet")
     get_parser.add_argument("name", help="The name of the snippet")
+    
+    # Subparser for the catalog command
+    logging.debug("Constructing catalog subparser")
+    get_parser = subparsers.add_parser("catalog", help="Return Keywords Catalog in snippet")
+
+    # Subparser for the search command
+    logging.debug("Constructing search subparser")
+    get_parser = subparsers.add_parser("search", help="Search a given string anywhere in their messages")
+    get_parser.add_argument("word", help="The string used to search")
 
     arguments = parser.parse_args()
     # Convert parsed arguments from Namespace to dictionary
@@ -68,6 +105,12 @@ def main():
     elif command == "get":
         snippet = get(**arguments)
         print("Retrieved snippet: {!r}".format(snippet))
+    elif command == "catalog":
+        catalog()
+        print("Retrieved Keywords")
+    elif command == "search":
+        word = search(**arguments)
+        print("Retrieved String: {!r}".format(word))
 
 if __name__ == "__main__":
     main()
