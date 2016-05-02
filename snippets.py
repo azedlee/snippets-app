f624bb4cefa988f4beaef89cd6709fbbf0a28204
Edward Lee <edwardlee0323@gmail.com>
2016-05-02 05:35:27 +0000

    Improving Error Handling with put and get

--- snippets.py
+++ snippets.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import logging
 import argparse
 import psycopg2
@@ -16,65 +15,13 @@ def put(name, snippet):
     """
     logging.info("Storing snippet {!r}: {!r}".format(name, snippet))
     cursor = connection.cursor()
-    command = "insert into snippets values (%s, %s)"
-    cursor.execute(command, (name, snippet))
-    connection.commit()
-    logging.debug("Snippet stored successfully.")
-    return name, snippet
-
-def get(name):
-    """
-    Retrieve the snippet with a given name.
-    """
-    logging.error("FIXME: Unimplemented - get({!r})".format(name))
-    return ""
-
-def main():
-    """Main function"""
-    logging.info("Constructing parser")
-    parser = argparse.ArgumentParser(description="Store and retrieve snippets of text")
-
-    subparsers = parser.add_subparsers(dest="command", help="Available commands")
-
-    # Subparser for the put command
-    logging.debug("Constructing put subparser")
-    put_parser = subparsers.add_parser("put", help="Store a snippet")
-    put_parser.add_argument("name", help="Name of the snippet")
-    put_parser.add_argument("snippet", help="Snippet text")
-
-    arguments = parser.parse_args()
-    # Convert parsed arguments from Namespace to dictionary
-    arguments = vars(arguments)
-    command = arguments.pop("command")
-
-    if command == "put":
-        name, snippet = put(**arguments)
-        print("Stored {!r} as {!r}".format(snippet, name))
-    elif command == "get":
-        snippet = get(**arguments)
-        print("Retrieved snippet: {!r}".format(snippet))
-
-if __name__ == "__main__":
-=======
-import logging
-import argparse
-import psycopg2
-
-logging.debug("Connecting to PostgreSQL")
-connection = psycopg2.connect(database="snippets")
-logging.debug("Database connection established.")
-
-# Set the log output file, and the log level
-logging.basicConfig(filename="snippets.log", level = logging.DEBUG)
-
-def put(name, snippet):
-    """
-    Store a snippet with an associated name.
-    """
-    logging.info("Storing snippet {!r}: {!r}".format(name, snippet))
-    cursor = connection.cursor()
-    command = "insert into snippets values (%s, %s)"
-    cursor.execute(command, (name, snippet))
+    try:
+        command = "insert into snippets values (%s, %s)"
+        cursor.execute(command, (name, snippet))
+    except psycopg2.IntegrityError as e:
+        connection.rollback()
+        command = "update snippets set message=%s where keyword=%s"
+        cursor.execute(command, (snippet, name))
     connection.commit()
     logging.debug("Snippet stored successfully.")
     return name, snippet
@@ -85,12 +32,16 @@ def get(name):
     """
     logging.info("Fetching snippet {!r}".format(name))
     cursor = connection.cursor()
-    command = "select keyword, message from snippets where keyword=(%s, )"
-    cursor.fetchone(command, (name, ))
+    command = "select keyword, message from snippets where keyword=%s"
+    cursor.execute(command, (name, ))
+    row = cursor.fetchone()
     connection.commit()
     logging.debug("Snippet successfully fetched.")
-    return name
-
+    if not row:
+        #no snippet was found with that name
+        return "404: Snippet Not Found"
+    return row[0]
+    
 
 def main():
     """Main function"""
@@ -105,6 +56,11 @@ def main():
     put_parser.add_argument("name", help="Name of the snippet")
     put_parser.add_argument("snippet", help="Snippet text")
 
+    # Subparser for the get command
+    logging.debug("Constructing get subparser")
+    get_parser = subparsers.add_parser("get", help="Retrieve a snippet")
+    get_parser.add_argument("name", help="The name of the snippet")
+
     arguments = parser.parse_args()
     # Convert parsed arguments from Namespace to dictionary
     arguments = vars(arguments)
@@ -118,5 +74,4 @@ def main():
         print("Retrieved snippet: {!r}".format(snippet))
 
 if __name__ == "__main__":
->>>>>>> bf9b2bf0af7cac7ff32fb6723ebd86cf085a178f
     main()
